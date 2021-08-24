321b830aaa41ae4fa3312147e370200de7e0b3e1
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-24 15:52:00 +0530

    ADDED : Computing Employee Monthly Wage By Using While Loop

--- EmpWage.js
+++ EmpWage.js
@@ -1,5 +1,5 @@
 console.log("Welcome To The Employee Wage Computation");
-//UC4-Computing The Employee Wage By Usimg For Loop
+//UC5-Computing The Monthly Employee Wage By Usimg While Loop
 {
  const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -7,6 +7,10 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
+const MAX_HOURS_IN_MONTH =160;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+
   function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -18,10 +22,11 @@ const NUM_OF_WORKING_DAYS=20;
         }
   }
 let empHrs=0;
-for(let day=0; day<NUM_OF_WORKING_DAYS;day++){
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs += getWorkingHours(empCheck);
+while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours " + empHrs +  " Emp Wage "+ empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days " + totalWorkingDays + " Hours " + totalEmpHrs +  " Emp Wage "+ empWage);
 }
