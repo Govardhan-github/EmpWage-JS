cf9a76005f9088497abcca4c6bbf2150e5d6e96b
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-24 21:43:07 +0530

    ADDED : Map Functions Are Added

--- EmpWage.js
+++ EmpWage.js
@@ -1,5 +1,5 @@
 console.log("Welcome To The Employee Wage Computation");
-//UC7-Computing The Monthly Employee Wage By Usimg Array
+//UC7-Computing The Monthly Employee Wage By Usimg Array Functions
 {
  const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -11,6 +11,7 @@ const MAX_HOURS_IN_MONTH =160;
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 let empDailyWageArr = new Array();
+let empDailyWageMap =  new Map();
 function calDailyWage(empHrs){
     return empHrs * WAGE_PER_HOUR;
 }
@@ -31,61 +32,11 @@ while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY
     empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays,calDailyWage(empHrs));
 }
-let empWage = calDailyWage(totalEmpHrs);
-console.log("Total Days " + totalWorkingDays + " Hours " + totalEmpHrs +  " Emp Wage "+ empWage);
-
-let totalEmpWage=0;
-function sum(dailyWage) {
-    totalEmpWage += dailyWage;
-}
-//UC-7A--Using For Each Loop For Emp Wage
-empDailyWageArr.forEach(sum);
-console.log("Total Days : " + totalWorkingDays + "Total Hours "  + totalEmpHrs + "Emp Wage " + totalEmpWage);
-//Reduce Function
+console.log(empDailyWageMap);
 function totalEmpWages(totalWage , dailyWage) {
     return totalWage + dailyWage;
 }
-console.log("Emp Wage With Reduce : " + empDailyWageArr.reduce(totalEmpWages,0));
-
-//UC-7B-Show The Day Along With Daily Wage Using Array Map Function
-let dailyCntr=0;
-function mapDayWithWage(dailyWage){
-    dailyCntr++;
-    return dailyCntr + "=" +dailyWage;
-}
-let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
-console.log("Daily Wage With Map");
-console.log(mapDayWithWageArr);
-//UC-7C-Show Days When Full Time Earned 
-function fullTimeWage(dailyWage) {
-    return dailyWage.includes("160");
-}
-let fullDayWageArr = mapDayWithWageArr.filter(fullTimeWage);
-console.log("Daily Wage Filter When FullTime Earned");
-console.log(fullDayWageArr);
-//UC-7D-Find The First Occurance When Full Time Was Earned
-function findFullTimeWage(dailyWage){
-    return dailyWage.includes("160");
-}
-console.log("First Time Full Time Wage Was Earned " + mapDayWithWageArr.find(findFullTimeWage));
-
-//UC-7E-Check If Every Element Of Full Time Wage Is Truely Holding Full Time Wage 
-function isAllFullTimeWage(dailyWage){
-    return dailyWage.includes("160");
-}
-console.log("Check All Elements Have Full Time Wage " +fullDayWageArr.every(isAllFullTimeWage));
-//UC-7F-Check If There Is Any Part Time Wage
-function isAnyPartTimeWage(dailyWage){
-    return dailyWage.includes("80");
-
-}
-console.log("ANY PartvTiem Wage " +mapDayWithWageArr.some(isAnyPartTimeWage));
-//UC-7G-Find The Num Of Days Employee Worked
-function totalDaysWorked(numOfDays,dailyWage){
-    if(dailyWage>0)
-        return numOfDays+1;
-    return numOfDays;    
-}
-console.log("Num Of Days Worked " +empDailyWageArr.reduce(totalDaysWorked , 0)); 
+console.log("Emp Wage Map TotalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalEmpWages,0));
 }
