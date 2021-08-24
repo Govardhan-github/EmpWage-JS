011f0b1d8b993338be22eb63b4347ddebcb1b3c4
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-24 15:41:48 +0530

    ADDED : Computing Employee Monthly Wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,21 +1,12 @@
 console.log("Welcome To The Employee Wage Computation");
-//UC1-Checking Employee Attendence
-{
-    const IS_ABSENT =0;
-    let empCheck = Math.floor(Math.random()*10) %2;
-    if(empCheck==IS_ABSENT){
-        console.log("Employee Is ABSENT");
-    }else {
-        console.log("Employee Is PRESENT");
-    }
-}
-//UC3-Computing The Employee Wage By Usimg Function
+//UC4-Computing The Employee Wage By Usimg For Loop
 {
  const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
   function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -27,8 +18,10 @@ const WAGE_PER_HOUR=20;
         }
   }
 let empHrs=0;
+for(let day=0; day<NUM_OF_WORKING_DAYS;day++){
 let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage "+ empWage);
+console.log("Hours " + empHrs +  " Emp Wage "+ empWage);
 }
